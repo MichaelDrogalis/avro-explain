903d1c04d20d97af5512f0b18ed40e9176dbbed5
Michael Drogalis <mjdrogalis@gmail.com>
2025-05-07 10:51:42 -0700

    No sign

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
+  :deploy-repositories [["clojars"  {:sign-releases false :url "https://clojars.org"}]]
+
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :test-paths ["test/clj"]
