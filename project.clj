92ff50188d0775a87b1e59f44c0e750f6895aef3
Michael Drogalis <mjdrogalis@gmail.com>
2025-05-07 10:30:42 -0700

    Project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject io.mdrogalis/avro-explain "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Better Avro error messages."
+  :url "https://github.com/MichaelDrogalis/avro-explain"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
