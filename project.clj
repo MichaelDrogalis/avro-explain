e20853a23f3ee82b7c650172a24ffbad1bc11972
Michael Drogalis <mjdrogalis@gmail.com>
2025-05-12 11:03:12 -0700

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mjdrogalis/avro-explain "0.1.0"
+(defproject org.clojars.mjdrogalis/avro-explain "0.1.1"
   :description "Better Avro error messages."
   :url "https://github.com/MichaelDrogalis/avro-explain"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
